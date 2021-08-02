aa6f72243a06003c356f1bddfda02d12d3a6524c
Messyo <messyoroboto@gmail.com>
2021-08-02 07:39:26 -0300

    Created unit test for find by id service

--- src/test/java/com/messyo/bookstoremanager/controller/BookControllerTest.java
+++ src/test/java/com/messyo/bookstoremanager/controller/BookControllerTest.java
@@ -3,29 +3,25 @@ package com.messyo.bookstoremanager.controller;
 import com.messyo.bookstoremanager.dto.BookDTO;
 import com.messyo.bookstoremanager.dto.MessageResponseDTO;
 import com.messyo.bookstoremanager.service.BookService;
-import com.messyo.bookstoremanager.utils.BookUtils;
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
-import java.net.URI;
-
-import static com.messyo.bookstoremanager.utils.BookUtils.*;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static com.messyo.bookstoremanager.utils.BookUtils.asJsonString;
+import static com.messyo.bookstoremanager.utils.BookUtils.createFakeBookDTO;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 public class BookControllerTest {
---
+++ src/test/java/com/messyo/bookstoremanager/service/BookServiceTest.java
@@ -0,0 +1,44 @@
+package com.messyo.bookstoremanager.service;
+
+import com.messyo.bookstoremanager.dto.BookDTO;
+import com.messyo.bookstoremanager.entity.Book;
+import com.messyo.bookstoremanager.repository.BookRepository;
+import com.messyo.bookstoremanager.utils.BookUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static com.messyo.bookstoremanager.utils.BookUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BookServiceTest {
+    private static final String BOOK_API_URL_PATH = "/api/v1/books";
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @InjectMocks
+    private BookService bookService;
+
+    @Test
+    void whenGivenExistingIdThenReturnThisBook() {
+        Book expectedFoundBook = createFakeBook();
+
+        when(bookRepository.findById(expectedFoundBook.getId())).thenReturn(Optional.of(expectedFoundBook));
+
+        BookDTO bookDTO = bookService.findById(expectedFoundBook.getId());
+
+        assertEquals(expectedFoundBook.getName(), bookDTO.getName());
+        assertEquals(expectedFoundBook.getIsbn(), bookDTO.getIsbn());
+        assertEquals(expectedFoundBook.getPublisherName(), bookDTO.getPublisherName());
+    }
+}
