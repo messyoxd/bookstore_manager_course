43489ae14f350617ae49d4afc2da2714984a8e37
Messyo <messyoroboto@gmail.com>
2021-08-02 07:22:17 -0300

    Developed get by id operation

--- src/main/java/com/messyo/bookstoremanager/controller/BookController.java
+++ src/main/java/com/messyo/bookstoremanager/controller/BookController.java
@@ -2,8 +2,6 @@ package com.messyo.bookstoremanager.controller;
 
 import com.messyo.bookstoremanager.dto.BookDTO;
 import com.messyo.bookstoremanager.dto.MessageResponseDTO;
-import com.messyo.bookstoremanager.entity.Book;
-import com.messyo.bookstoremanager.repository.BookRepository;
 import com.messyo.bookstoremanager.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -24,5 +22,10 @@ public class BookController {
     @PostMapping
     public MessageResponseDTO create(@RequestBody @Valid BookDTO bookDTO){
         return bookService.create(bookDTO);
+    }
+
+    @GetMapping("/{id}")
+    public BookDTO findById(@PathVariable Long id){
+        return bookService.findById(id);
     }
 }
--- src/main/java/com/messyo/bookstoremanager/service/BookService.java
+++ src/main/java/com/messyo/bookstoremanager/service/BookService.java
@@ -8,6 +8,8 @@ import com.messyo.bookstoremanager.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class BookService {
     private BookRepository bookRepository;
@@ -25,5 +27,10 @@ public class BookService {
         return MessageResponseDTO.builder()
                 .message("Book created with ID " + savedBook.getId())
                 .build();
+    }
+
+    public BookDTO findById(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        return bookMapper.toDTO(optionalBook.get());
     }
 }
